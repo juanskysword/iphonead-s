6695165c1e2fff03f9a000e0efa509c9a432e6ce
Juan Tavera <juanskysword@gmail.com>
2022-02-09 15:51:47 -0800

    start

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+const port = process.envPORT || 4000
+
+app.listen(4000, () => console.log(`server running on 4000`))
